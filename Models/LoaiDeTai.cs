1e207bb49d4973ce6c01ac788e10890a99bfc2ad
Ziop21 <103240614+Ziop21@users.noreply.github.com>
2022-12-08 19:04:41 +0700

    spint 2 - Phu

--- Models/LoaiDeTai.cs
+++ Models/LoaiDeTai.cs
@@ -25,6 +25,8 @@ namespace QuanLyDeTai.Models
         public string tenLoaiDeTai { get; set; }
         public string nienKhoa { get; set; }
         public Nullable<int> hocKy { get; set; }
+        public Nullable<System.DateTime> tgDangKy { get; set; }
+        public Nullable<System.DateTime> tgKetThuc { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<DeTai> DeTais { get; set; }
---
+++ ViewsModel/NhomsViewsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuanLyDeTai.Models;
+
+namespace QuanLyDeTai.ViewsModel
+{
+    public class NhomsViewsModel
+    {
+        public List<SinhVien> danhsach { get; set; }
+        public int? truongNhom { get; set; }
+
+        public NhomsViewsModel()
+        {
+            this.danhsach = new List<SinhVien>();
+            
+        }
+        public NhomsViewsModel(List<SinhVien> danhsach, int? truongNhom)
+        {
+            this.danhsach = danhsach;
+            this.truongNhom = truongNhom;
+          
+        }
+    }
+}
